b995678cec8dc5c23574bcb0b64170086c268fc2
Maksym Yenenko <m.enenko@gmail.com>
2019-11-01 16:25:22 +0200

    add reducer to combine reducers

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,3 +1,8 @@
 import { combineReducers } from "redux";
+import stock from "./stock";
 
-export const rootReducer = combineReducers({});
+export const rootReducer = combineReducers({
+  stock
+});
+
+export type AppState = ReturnType<typeof rootReducer>;
