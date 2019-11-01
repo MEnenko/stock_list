e01b7b63f7a1b64e56ce9586e64fb6c8f7e4f6af
Maksym Yenenko <m.enenko@gmail.com>
2019-11-01 16:22:05 +0200

    add page home

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,7 +1,70 @@
 import React, { useState } from "react";
+import Search from "../ui/Search";
+import { Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  loadStock,
+  loadFavoriteSymbolList,
+  resetStock,
+  markAsFavorite,
+  removeTheMarkAsFavorite
+} from "../../actions/stock";
+import StockList from "../ui/StockList";
+import { AppState } from "../../reducers/index";
 
 export const Home: React.FC = () => {
-  return <div>page1</div>;
+  const [searchText, setSearchText] = useState("");
+  const dispatch = useDispatch();
+  const stocks = useSelector(({ stock }: AppState) => stock.list);
+  const favoritesStocks = useSelector(
+    ({ stock }: AppState) => stock.favoriteSymbolList
+  );
+
+  const handleChange = (text: string) => {
+    if (!searchText) {
+      dispatch(resetStock());
+    }
+
+    setSearchText(text);
+  };
+
+  const handleSubmit = () => {
+    if (searchText) {
+      dispatch(loadStock(searchText));
+      dispatch(loadFavoriteSymbolList());
+    }
+  };
+
+  const handleMarkAsFavoriteStock = (symbol: string) => {
+    if (favoritesStocks.includes(symbol)) {
+      return dispatch(removeTheMarkAsFavorite(symbol));
+    }
+
+    dispatch(markAsFavorite(symbol));
+  };
+
+  return (
+    <div>
+      <Row>
+        <Col>
+          <Search
+            value={searchText}
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <StockList
+            stocks={stocks}
+            favoritesStocks={favoritesStocks}
+            onAddStockToFavorites={handleMarkAsFavoriteStock}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Home;
