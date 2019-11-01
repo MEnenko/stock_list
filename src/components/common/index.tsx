9f0459d79e5d60f8729fefd47955b0c3205d696e
Maksym Yenenko <m.enenko@gmail.com>
2019-11-01 16:22:35 +0200

    change pages in common file

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -1,24 +1,25 @@
-import React, { Suspense } from "react";
-import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../pages/Home";
+import Favorites from "../pages/Favorites";
 import { Nav } from "react-bootstrap";
 
 const Cloud: React.FC = () => (
   <Router>
     <div>
-      <Nav variant="tabs" defaultActiveKey="/home">
+      <Nav variant="tabs" defaultActiveKey="/#home">
         <Nav.Item>
-          <Nav.Link href="/#home">Home</Nav.Link>
+          <Nav.Link href="/#home">Watch list</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="selected" href="/#selected">
-            Selected
+          <Nav.Link eventKey="selected" href="/#favorites">
+            Favorites
           </Nav.Link>
         </Nav.Item>
       </Nav>
       <Switch>
         <Route exact path="/home" component={Home} />
-        <Route exact path="/selected" component={Home} />
+        <Route path="/favorites" component={Favorites} />
       </Switch>
     </div>
   </Router>
