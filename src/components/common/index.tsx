6e409486c62eab881eae4971028a5169893b075a
Maksym Yenenko <m.enenko@gmail.com>
2019-11-04 10:04:02 +0200

    add show latest upload

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -3,24 +3,33 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../pages/Home";
 import Favorites from "../pages/Favorites";
 import { Nav, Navbar } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { AppState } from "../../reducers/index";
+import { format } from "date-fns";
 
-const Cloud: React.FC = () => (
-  <Router>
-    <div>
-      <Navbar bg="light" variant="light">
-        <Nav className="mr-auto">
-          <Nav.Link href="/#home">Watch list</Nav.Link>
-          <Nav.Link eventKey="selected" href="/#favorites">
-            Favorites
-          </Nav.Link>
-        </Nav>
-      </Navbar>
-      <Switch>
-        <Route exact path="/home" component={Home} />
-        <Route path="/favorites" component={Favorites} />
-      </Switch>
-    </div>
-  </Router>
-);
+const Cloud: React.FC = () => {
+  const lastUpdatedAt = useSelector(({ stock }: AppState) => stock.updatedAt);
+
+  return (
+    <Router>
+      <div>
+        <Navbar bg="light" variant="light" className="justify-content-between">
+          <Nav className="mr-auto">
+            <Nav.Link href="/#home">Watch list</Nav.Link>
+            <Nav.Link eventKey="selected" href="/#favorites">
+              Favorites
+            </Nav.Link>
+          </Nav>
+          <p>Last update - {format(lastUpdatedAt, "kk:mm:ss - MM/dd/yyyy")}</p>
+        </Navbar>
+
+        <Switch>
+          <Route exact path="/home" component={Home} />
+          <Route path="/favorites" component={Favorites} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default Cloud;
--- src/reducers/stock.ts
+++ src/reducers/stock.ts
@@ -1,20 +1,24 @@
 import {
   SET_STOCK,
   SET_FAVORITES_STOCKS_SYMBOL,
-  SET_FAVORITE_STOCK_LIST
+  SET_FAVORITE_STOCK_LIST,
+  SET_LATEST_UPDATE_TIME
 } from "../actions/types";
 import { Reducer } from "redux";
 import { IStock, IFavoritesStocks } from "types";
+const KEY_FAVORITE = "favorite-list";
 
 export interface IStockState {
   readonly list: IStock[];
+  updatedAt: Date;
   favoriteSymbolList: string[];
   favoriteStockList: IFavoritesStocks[];
 }
 
 const initialState: IStockState = {
   list: [],
-  favoriteSymbolList: [],
+  updatedAt: new Date(),
+  favoriteSymbolList: JSON.parse(localStorage.getItem(KEY_FAVORITE) || "[]"),
   favoriteStockList: []
 };
 
@@ -28,14 +32,18 @@ const reducer: Reducer<IStockState> = (state = initialState, action) => {
     case SET_FAVORITES_STOCKS_SYMBOL:
       return {
         ...state,
-        favoriteSymbolList: action.favoriteSymbolList,
-        favoriteStockList: []
+        favoriteSymbolList: action.favoriteSymbolList
       };
     case SET_FAVORITE_STOCK_LIST:
       return {
         ...state,
         favoriteStockList: action.favoriteStockList,
         list: []
+      };
+    case SET_LATEST_UPDATE_TIME:
+      return {
+        ...state,
+        updatedAt: new Date()
       };
     default:
       return state;
