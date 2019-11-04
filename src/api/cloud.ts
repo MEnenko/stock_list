26fb9593cd7c0658195b2d5068423b803fe57aa8
Maksym Yenenko <m.enenko@gmail.com>
2019-11-04 10:02:53 +0200

    add url to env

--- src/api/cloud.ts
+++ src/api/cloud.ts
@@ -1,10 +1,8 @@
 import { IStock, IFavoritesStocks } from "types";
 
-const url = "https://cloud.iexapis.com/stable/stock";
-
 export const getStockBySearch = async (symbol: string): Promise<IStock[]> => {
   const res = await fetch(
-    `${url}/${symbol}/quote?filter=symbol,companyName&token=${process.env.REACT_APP_TOKEN}`
+    `${process.env.REACT_APP_URL}/${symbol}/quote?filter=symbol,companyName&token=${process.env.REACT_APP_TOKEN}`
   );
 
   return await res.json();
@@ -17,7 +15,7 @@ export const getFavoritesStockBySymbols = async (
   const filter = ["symbol", "companyName", "latestPrice"].join(",");
 
   const res = await fetch(
-    `${url}/market/batch?symbols=${symbols}&types=quote&filter=${filter}&token=${process.env.REACT_APP_TOKEN}`
+    `${process.env.REACT_APP_URL}/market/batch?symbols=${symbols}&types=quote&filter=${filter}&token=${process.env.REACT_APP_TOKEN}`
   );
 
   const favoriteStockList = Object.entries(await res.json()).map(
