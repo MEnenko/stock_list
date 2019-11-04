24a95741f0d95ac5c961edef650ac061d844f7ce
Maksym Yenenko <m.enenko@gmail.com>
2019-11-04 12:06:22 +0200

    add process placeholder to form url

--- src/api/cloud.ts
+++ src/api/cloud.ts
@@ -1,9 +1,21 @@
 import { IStock, IFavoritesStocks } from "types";
 
+export const processPlaceholder = (url: any, data: object) => {
+  debugger;
+  return Object.entries(data).reduce((prevValue, [key, value]) => {
+    const reg = new RegExp(`{${key}}`);
+    return prevValue.replace(reg, value);
+  }, url);
+};
+
 export const getStockBySearch = async (symbol: string): Promise<IStock[]> => {
-  const res = await fetch(
-    `${process.env.REACT_APP_URL}/${symbol}/quote?filter=symbol,companyName&token=${process.env.REACT_APP_TOKEN}`
-  );
+  const url = processPlaceholder(process.env.REACT_APP_URL_GET_LIST, {
+    symbol: symbol,
+    filter: "symbol,companyName",
+    token: process.env.REACT_APP_TOKEN
+  });
+
+  const res = await fetch(url);
 
   return await res.json();
 };
@@ -14,9 +26,13 @@ export const getFavoritesStockBySymbols = async (
   const symbols = symbolsList.join(",");
   const filter = ["symbol", "companyName", "latestPrice"].join(",");
 
-  const res = await fetch(
-    `${process.env.REACT_APP_URL}/market/batch?symbols=${symbols}&types=quote&filter=${filter}&token=${process.env.REACT_APP_TOKEN}`
-  );
+  const url = processPlaceholder(process.env.REACT_APP_URL_GET_FAVORITE_LIST, {
+    symbols,
+    filter,
+    token: process.env.REACT_APP_TOKEN
+  });
+
+  const res = await fetch(url);
 
   const favoriteStockList = Object.entries(await res.json()).map(
     ([key, value]: any) => value.quote
