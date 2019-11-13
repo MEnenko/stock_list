4c34b0e2e3b4e1bcf3214e5837186b18d1bf718b
Maksym Yenenko <m.enenko@gmail.com>
2019-11-13 16:22:59 +0200

    remove react fragment

--- src/components/ui/StockItem/index.tsx
+++ src/components/ui/StockItem/index.tsx
@@ -10,35 +10,33 @@ export interface IProps {
 }
 
 const StockItem: React.FC<IProps> = ({ stock, isFavorites, onClick }) => (
-  <React.Fragment>
-    <tr>
-      <td className="col-md-3">
-        <OverlayTrigger
-          key="bottom"
-          placement="bottom"
-          overlay={
-            <Tooltip id="bottom">
-              There is such paper and this is {stock.companyName} stock
-            </Tooltip>
-          }
-        >
-          <Button variant="light" block>
-            {stock.symbol}
-          </Button>
-        </OverlayTrigger>
-      </td>
-      <td align="right">
-        <button
-          className={classNames("btn btn-default text-secondary", [
-            { "text-warning": isFavorites }
-          ])}
-          onClick={onClick.bind(null, stock.symbol)}
-        >
-          ★
-        </button>
-      </td>
-    </tr>
-  </React.Fragment>
+  <tr>
+    <td className="col-md-3">
+      <OverlayTrigger
+        key="bottom"
+        placement="bottom"
+        overlay={
+          <Tooltip id="bottom">
+            There is such paper and this is {stock.companyName} stock
+          </Tooltip>
+        }
+      >
+        <Button variant="light" block>
+          {stock.symbol}
+        </Button>
+      </OverlayTrigger>
+    </td>
+    <td align="right">
+      <button
+        className={classNames("btn btn-default text-secondary", [
+          { "text-warning": isFavorites }
+        ])}
+        onClick={onClick.bind(null, stock.symbol)}
+      >
+        ★
+      </button>
+    </td>
+  </tr>
 );
 
 export default StockItem;
