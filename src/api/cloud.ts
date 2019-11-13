96a2b9cef18725437b0dbd66a900b71b45e52900
Maksym Yenenko <m.enenko@gmail.com>
2019-11-13 10:17:45 +0200

    add default url if don't indicated env

--- src/api/cloud.ts
+++ src/api/cloud.ts
@@ -1,5 +1,10 @@
 import { IStock, IFavoritesStocks } from "types";
 
+const URL_GET_LIST =
+  "https://cloud.iexapis.com/stable/stock/{symbol}/quote?filter={filter}&token={token}";
+const URL_GET_FAVORITE_LIST =
+  "https://cloud.iexapis.com/stable/stock/market/batch?symbols={symbols}&types=quote&filter={filter}&token={token}";
+
 export const processPlaceholder = (url: any, data: object) => {
   return Object.entries(data).reduce((prevValue, [key, value]) => {
     const reg = new RegExp(`{${key}}`);
@@ -8,11 +13,14 @@ export const processPlaceholder = (url: any, data: object) => {
 };
 
 export const getStockBySearch = async (symbol: string): Promise<IStock[]> => {
-  const url = processPlaceholder(process.env.REACT_APP_URL_GET_LIST, {
-    symbol: symbol,
-    filter: "symbol,companyName",
-    token: process.env.REACT_APP_TOKEN
-  });
+  const url = processPlaceholder(
+    process.env.REACT_APP_URL_GET_LIST || URL_GET_LIST,
+    {
+      symbol: symbol,
+      filter: "symbol,companyName",
+      token: process.env.REACT_APP_TOKEN
+    }
+  );
 
   const res = await fetch(url);
 
@@ -25,11 +33,14 @@ export const getFavoritesStockBySymbols = async (
   const symbols = symbolsList.join(",");
   const filter = ["symbol", "companyName", "latestPrice"].join(",");
 
-  const url = processPlaceholder(process.env.REACT_APP_URL_GET_FAVORITE_LIST, {
-    symbols,
-    filter,
-    token: process.env.REACT_APP_TOKEN
-  });
+  const url = processPlaceholder(
+    process.env.REACT_APP_URL_GET_FAVORITE_LIST || URL_GET_FAVORITE_LIST,
+    {
+      symbols,
+      filter,
+      token: process.env.REACT_APP_TOKEN
+    }
+  );
 
   const res = await fetch(url);
 
